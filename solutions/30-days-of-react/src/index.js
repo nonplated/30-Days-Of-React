3daa94163de3e312ef34f788eeb03f6b835803c2
firestatus <40839417+nonplated@users.noreply.github.com>
2022-11-02 18:19:50 +0100

    add redux example inc and dec

--- solutions/30-days-of-react/src/index.js
+++ solutions/30-days-of-react/src/index.js
@@ -3,6 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'
+
+// https://www.youtube.com/watch?v=CVpUuw9XSjY
+// ACTION
+const increment = () => {
+    return {
+        type: 'INCREMENT',
+    }
+}
+const decrement = () => {
+    return {
+        type: 'DECREMENT',
+    }
+}
+
+// REDUCER
+const counter = (state = 0, action) => {
+    switch (action.type){
+        case 'INCREMENT':
+            return state+1
+        case 'DECREMENT':
+            return state-1
+    }
+}
+
+let store = createStore(counter);
+store.subscribe( () => console.log(store.getState()));
+
+// DISPATCH
+store.dispatch(increment())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
